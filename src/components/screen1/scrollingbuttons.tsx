dbcfaa20d908338fba643f133adbb65c9bd2b7c8
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2023-03-04 17:32:01 +0100

    fett-note-v2

--- src/components/screen1/scrollingbuttons.tsx
+++ src/components/screen1/scrollingbuttons.tsx
@@ -19,34 +19,32 @@ export const ScrollingBnts: React.FC<CoinBtns> = ({data}) => {
     const secondHalf = data.splice(-middleIndex);
     return(
         <>
-            <Marquee direction="left" className={styles.scroll_btns}>
+            <Marquee direction="left" pauseOnClick={true} className={styles.scroll_btns}>
                 {firstHalf.map((btn, ndx) => (
                     <div key={ndx} className={styles.crypto_bnt}>
-                        <Link href={`/trade/${btn.coin_id}`}>
-                            <a>
-                                <Image 
-                                    src={btn.coin_logo}
-                                    alt={btn.coin_name}
-                                    sizes={`20px`}
-                                />
-                                <h5>{btn.coin_name}</h5>
-                            </a>
+                        <Link href={`/trade?crypto=${btn.coin_id}`}>
+                  
+                            <Image 
+                                src={btn.coin_logo}
+                                alt={btn.coin_name}
+                                sizes={`20px`}
+                            />
+                            <h5>{btn.coin_name}</h5>
+        
                         </Link>
                     </div>
                 ))}
             </Marquee>
-            <Marquee direction="right" className={styles.scroll_btns}>
+            <Marquee direction="right" pauseOnClick={true} className={styles.scroll_btns}>
                 {secondHalf.map((btn, ndx) => (
                     <div key={ndx} className={styles.crypto_bnt}>
                         <Link href={`/trade?crypto=${btn.coin_id}`}>
-                            <a>
-                                <Image 
-                                    src={btn.coin_logo}
-                                    alt={btn.coin_name}
-                                    sizes={`20px`}
-                                />
-                                <h5>{btn.coin_name}</h5>
-                            </a>
+                            <Image 
+                                src={btn.coin_logo}
+                                alt={btn.coin_name}
+                                sizes={`20px`}
+                            />
+                            <h5>{btn.coin_name}</h5>
                         </Link>
                     </div>
                 ))}
